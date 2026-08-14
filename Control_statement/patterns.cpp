9f31dfba902a5ef85887b4d8daedfba63f82eb68
Rishika Bishnoi <rishikabishnoi2704@gmail.com>
2026-08-14 19:01:15 +0530

    conditional_statement

--- Control_statement/patterns.cpp
+++ Control_statement/patterns.cpp
@@ -125,3 +125,188 @@ int main() {
 }
 
 
+
+
+//hollow triangle pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=n-i;j>=1;j--){
+            cout<<" ";
+        }
+        for(int j=1;j<=(2*i-1);j++){
+            if(j==1 || j==(2*i-1) || i==n){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+//hollow triangle pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(j==1 || j==i || i==n){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+//hollow square pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==1 || i==n || j==1 || j==n){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+
+//butterfly pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){ 
+            cout<<"*";
+        }
+        for(int j=1;j<=2*(n-i);j++){   
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
+//simple number pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){ 
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+
+
+//reversed number pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        int a=i;
+        for(int j=1;j<=i;j++){ 
+            cout<<a;
+            a--;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+
+
+//inverted number triangle pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){ 
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}   
+
+
+
+//floyd's triangle pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int a=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){ 
+            cout<<a;
+            a++;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+
+

f34145556a08113753d199000c142a5497eb6934
Rishika Bishnoi <rishikabishnoi2704@gmail.com>
2026-08-17 19:17:50 +0530

    pattern

--- Control_statement/patterns.cpp
+++ Control_statement/patterns.cpp
@@ -310,3 +310,26 @@ int main() {
 
 
 
+//pascal's triangle pattern
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        int value=1;
+        for(int j=0; j<n-i; j++){
+            cout<<" ";
+        }
+        for(int k=0; k<=i; k++){
+            cout<<value<<" ";
+            value=value*(i-k)/(k+1);
+            
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+

a4f8a6b315712a64fcf36981174837557e42af45
Rishika Bishnoi <rishikabishnoi2704@gmail.com>
2026-09-05 18:23:41 +0530

    arrays

--- array.cpp
+++ array.cpp
@@ -76,3 +76,136 @@ int main()
     cout<<"Largest element in array:"<<max;
     return 0;
 }
+
+
+
+
+
+// 4. maximum subarray sum :
+#include <iostream>
+using namespace std;
+ 
+int maxSumSubarray(vector<int> &arr, int k){
+    int windowSum = 0;
+ 
+    for(int i=0; i<k; i++){
+        windowSum += arr[i];
+    }
+ 
+    int maxSum =  windowSum;
+ 
+    for(int i=k; i<arr.size(); i++){
+        windowSum += arr[i];
+        windowSum -= arr[i-k];
+        maxSum = max(maxSum, windowSum);
+    }
+    return maxSum;
+}
+ 
+int main(){
+     vector<int> arr = {2, 1, 5, 1, 3, 2};
+ 
+    cout<<maxSumSubarray(arr, 3);
+ 
+    return 0;
+}
+
+
+
+// 5. minimum subarray sum :
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int left = 0, right = 0;
+        int shortest = INT_MAX;
+        int sum = 0;
+       
+        // variable size sliding window: 2-pointer
+        while(right<n){
+            sum += nums[right]; //add current element to window
+            if(sum >= target){
+                // skip all left elements til sum < target (to find the smallest window)
+                while(sum >= target){
+                    sum -= nums[left];
+                    left++;
+                }
+                shortest = min(shortest, right-left+2); //update the smallest window size
+            }
+            right++;
+        }
+        return shortest==INT_MAX?0:shortest;
+    }
+   
+};
+
+
+
+
+// 6. smallest subarray with sum greater than a given value :
+#include<iostream>
+using namespace std;
+ 
+int smallestSubarray(vector<int> &arr, int target){
+    int n = arr.size();
+    int i = 0;
+    int sum = 0;
+    int min_Len = INT_MAX;
+   
+    //expand the window using right pointer 'j'
+ 
+    for(int j=0; j<n; j++){
+        sum += arr[j];
+        while(sum >= target){
+            min_Len = min(min_Len, j-i+1);
+            sum = sum - arr[i];
+            i++;
+        }
+    }
+    return min_Len;
+}
+ 
+int main(){
+ 
+    vector<int> arr = {1, 2, 3, 4, 5, 5, 7, 8};
+    int target = 15;
+ 
+    cout<<smallestSubarray(arr, target);
+ 
+    return 0;
+}
+
+
+
+//kadane's algorithm(brute force) : maximum subarray sum
+#include<iostream>
+using namespace std;
+ 
+class Solution{
+    public:
+        int kadane(vector<int> &arr){
+            int n = arr.size();
+            int maxv = INT_MIN;
+            for(int i=0; i<n; i++){
+                int currSum = 0;
+                for(int j=i; j<n; j++){
+                    currSum += arr[j];
+                    maxv = max(currSum, maxv);
+                }
+            }
+            return maxv;
+        }
+};
+ 
+int main(){
+ 
+    Solution obj;
+ 
+    vector<int> vec = {2, 3, 5, -2, 7, -4};
+   
+    cout<<obj.kadane(vec);
+ 
+    return 0;
+}

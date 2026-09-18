428783bcd2843bbe5a4c1a0f7b6f6514dd1f9230
Rishika Bishnoi <rishikabishnoi2704@gmail.com>
2026-09-19 00:03:04 +0530

    leetcode questions

--- leetcode_question.cpp
+++ leetcode_question.cpp
@@ -332,3 +332,29 @@ public:
         return ans;
     }
 };
+
+
+
+//367. Valid Perfect Square
+// Example 1:
+// Input: num = 16
+// Output: true
+// Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num == 0) return false;
+        int left = 1, right = num;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid == num / mid && num % mid == 0) {
+                return true;
+            } else if (mid < num / mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+};

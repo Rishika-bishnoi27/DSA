83a52191296db16651aeb630db6367a4bcd2f64e
Rishika Bishnoi <rishikabishnoi2704@gmail.com>
2026-09-18 23:45:08 +0530

    leetcode question

--- leetcode_question.cpp
+++ leetcode_question.cpp
@@ -301,3 +301,34 @@ class Solution {
     return ans;
   }
 };
+
+
+
+//69. 69. Sqrt(x)
+// Example 1:
+
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0 || x==1){
+            return x;
+        }
+        int left=0;
+        int right=x;
+        int ans=0;
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(mid<=x/mid){
+                ans=mid;
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+};

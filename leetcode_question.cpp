f90100ddacf11206537eef086c859bcd79179a2d
Rishika Bishnoi <rishikabishnoi2704@gmail.com>
2026-09-19 17:44:16 +0530

    leetcode question

--- leetcode_question.cpp
+++ leetcode_question.cpp
@@ -358,3 +358,83 @@ public:
         return false;
     }
 };
+
+
+
+//51. N-Queens
+// Example 1:
+// Input: n = 4
+// Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+// Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+class Solution {
+public:
+
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+
+        // Check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check left diagonal
+        for (int i = row - 1, j = col - 1;
+             i >= 0 && j >= 0;
+             i--, j--) {
+
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check right diagonal
+        for (int i = row - 1, j = col + 1;
+             i >= 0 && j < n;
+             i--, j++) {
+
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void solveNQueen(vector<string>& board, int row,
+                     int n, vector<vector<string>>& ans) {
+
+        // If all rows are completed
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        // Try every column
+        for (int col = 0; col < n; col++) {
+
+            if (isSafe(board, row, col, n)) {
+
+                // Place queen
+                board[row][col] = 'Q';
+
+                // Move to next row
+                solveNQueen(board, row + 1, n, ans);
+
+                // Backtracking
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+
+        vector<vector<string>> ans;
+
+        vector<string> board(n, string(n, '.'));
+
+        solveNQueen(board, 0, n, ans);
+
+        return ans;
+    }
+};

5df997658501d31ea22fe2510061394e40e9f102
Rishika Bishnoi <rishikabishnoi2704@gmail.com>
2026-09-21 23:26:48 +0530

    leetcode question

--- leetcode_question.cpp
+++ leetcode_question.cpp
@@ -479,3 +479,47 @@ public:
         return head;
     }
 };
+
+
+
+
+
+
+// 20. Valid Parentheses
+// Example 1:
+// Input: s = "([])"
+// Output: true
+
+// Example 2:
+// Input: s = "([)]"
+// Output: false
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char ch:s){
+            if(ch=='(' || ch=='[' || ch=='{' ){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                else{
+                char top=st.top();
+                st.pop();
+                if(ch==')' && top!='('){
+                    return false;
+                }
+                if(ch==']' && top!='['){
+                    return false;
+                }
+                if(ch=='}' && top!='{'){
+                    return false;
+                }
+                }
+            }
+        }
+        return st.empty();
+    }
+};

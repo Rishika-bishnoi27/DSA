4bb9d4665a879936295a88a9c28d001eec7ff9e9
Rishika Bishnoi <rishikabishnoi2704@gmail.com>
2026-09-25 23:28:12 +0530

    leetcode question

--- leetcode_question.cpp
+++ leetcode_question.cpp
@@ -762,3 +762,30 @@ public:
         return vec;
     }
 };
+
+
+
+
+//122. Best Time to Buy and Sell Stock II
+// Example 1:
+// Input: prices = [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+// Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+// Total profit is 4 + 3 = 7.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+
+        // Capture every increasing segment as profit.
+        for (int i = 1; i < static_cast<int>(prices.size()); ++i) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+};

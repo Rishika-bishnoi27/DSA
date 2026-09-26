1af2eee162891f75e853e98814171f7c173e6c7d
Rishika Bishnoi <rishikabishnoi2704@gmail.com>
2026-09-26 23:52:46 +0530

    leetcode question

--- leetcode_question.cpp
+++ leetcode_question.cpp
@@ -845,3 +845,20 @@ public:
         return true;
     }
 };
+
+
+//or
+
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        set<char> st;
+
+        for(char ch : sentence) {
+            st.insert(ch);
+        }
+
+        return st.size() == 26;
+    }
+};
